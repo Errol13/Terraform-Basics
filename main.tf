1f20319492275fd209526eb448c8d3c3104820e5
Errol13 <erroljohncelis13@gmail.com>
2025-06-26 12:58:53 +0800

    terraform initial conf

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "terraform_basics" {
+  bucket = "terraform-basics-101-ejc"
+}
---
+++ terraform.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.65.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+
